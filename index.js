129fd242a4d826f470d930485b559ce8680c9f8b
Sebastian <sgha10@hotmail.com>
2020-07-31 15:21:42 -0600

    more

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const sortem = require('./sortem');
 
-console.log(sortem.mergeSort([20,14,5,9,1,2,78,12,41,-12,4,6,-99]));
+console.log(sortem.mergeSort([20, 14, 5, 9, 1, 2, 78, 12, 41, -12, 4, 6, -99]));
+console.log(sortem.selectionSort([20, 14, 5, 9, 1, 2, 78, 12, 41, -12, 4, 6, -99]));
+console.log(sortem.quickSort([20, 14, 5, 9, 1, 2, 78, 12, 41, -12, 4, 6, -99]));
+console.log(sortem.insertionSort([20, 14, 5, 9, 1, 2, 78, 12, 41, -12, 4, 6, -99]));
+console.log(sortem.bubbleSort([20, 14, 5, 9, 1, 2, 78, 12, 41, -12, 4, 6, -99]));
